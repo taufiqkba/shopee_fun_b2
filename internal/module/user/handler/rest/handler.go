f4b2704f4d91167f0cd4f4309161e0d4b0287df8
taufiqkba <taufiqkurniawan9991@gmail.com>
2024-08-20 18:32:55 +0700

    from codebase

---
+++ internal/module/user/handler/rest/handler.go
@@ -0,0 +1,126 @@
+package rest
+
+import (
+	"codebase-app/internal/adapter"
+	integOauth "codebase-app/internal/integration/oauth2google"
+	"codebase-app/internal/middleware"
+	"codebase-app/internal/module/user/entity"
+	"codebase-app/internal/module/user/ports"
+	"codebase-app/internal/module/user/repository"
+	"codebase-app/internal/module/user/service"
+	"codebase-app/pkg/errmsg"
+	"codebase-app/pkg/response"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/rs/zerolog/log"
+)
+
+type userHandler struct {
+	service ports.UserService
+}
+
+func NewUserHandler(o integOauth.Oauth2googleContract) *userHandler {
+	var handler = new(userHandler)
+
+	repo := repository.NewUserRepository(adapter.Adapters.ShopeefunPostgres)
+	service := service.NewUserService(repo, o)
+
+	handler.service = service
+
+	return handler
+}
+
+func (h *userHandler) Register(router fiber.Router) {
+	router.Post("/register", h.register)
+	router.Post("/login", h.login)
+	router.Get("/profile", middleware.AuthBearer, h.profile)
+
+	router.Get("/oauth/google/url", h.oauthGoogleUrl)
+}
+
+func (h *userHandler) register(c *fiber.Ctx) error {
+	var (
+		req = new(entity.RegisterRequest)
+		ctx = c.Context()
+		v   = adapter.Adapters.Validator
+	)
+
+	if err := c.BodyParser(req); err != nil {
+		log.Warn().Err(err).Msg("handler::register - Failed to parse request body")
+		return c.Status(fiber.StatusBadRequest).JSON(response.Error(err))
+	}
+
+	if err := v.Validate(req); err != nil {
+		log.Warn().Err(err).Msg("handler::register - Invalid request body")
+		code, errs := errmsg.Errors(err, req)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	res, err := h.service.Register(ctx, req)
+	if err != nil {
+		code, errs := errmsg.Errors[error](err)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(response.Success(res, ""))
+}
+
+func (h *userHandler) login(c *fiber.Ctx) error {
+	var (
+		req = new(entity.LoginRequest)
+		ctx = c.Context()
+		v   = adapter.Adapters.Validator
+	)
+
+	if err := c.BodyParser(req); err != nil {
+		log.Warn().Err(err).Msg("handler::login - Failed to parse request body")
+		return c.Status(fiber.StatusBadRequest).JSON(response.Error(err))
+	}
+
+	if err := v.Validate(req); err != nil {
+		log.Warn().Err(err).Msg("handler::login - Invalid request body")
+		code, errs := errmsg.Errors(err, req)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	res, err := h.service.Login(ctx, req)
+	if err != nil {
+		code, errs := errmsg.Errors[error](err)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response.Success(res, ""))
+}
+
+func (h *userHandler) profile(c *fiber.Ctx) error {
+	var (
+		req   = new(entity.ProfileRequest)
+		ctx   = c.Context()
+		local = middleware.Locals{}
+		l     = local.GetLocals(c)
+	)
+
+	req.UserId = l.GetUserId()
+
+	res, err := h.service.Profile(ctx, req)
+	if err != nil {
+		code, errs := errmsg.Errors[error](err)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response.Success(res, ""))
+}
+
+func (h *userHandler) oauthGoogleUrl(c *fiber.Ctx) error {
+	var (
+		ctx = c.Context()
+	)
+
+	resp, err := h.service.GetOauthGoogleUrl(ctx)
+	if err != nil {
+		code, errs := errmsg.Errors[error](err)
+		return c.Status(code).JSON(response.Error(errs))
+	}
+
+	return c.Status(fiber.StatusOK).JSON(response.Success(resp, ""))
+}
